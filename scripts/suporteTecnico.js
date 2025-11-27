9fe69ffc31153ce9813ac0e06417865751053231
Francisco C <debian.programmer.francisco@gmail.com>
2025-11-27 13:12:10 -0300

    adicionando links de referencia ao Post

--- scripts/suporteTecnico.js
+++ scripts/suporteTecnico.js
@@ -6,8 +6,8 @@ artigos.forEach(artigo => {
     dado = artigo.dataset.id;
     if (dado === 'impGpeditWin1011PRO') {
       window.location.replace("/pages/projetos/suporteTecnico/impGpeditWin1011PRO/index.html");
-    } else if (dado === 'cmdWin1011ReparoSO') {
-      window.location.replace("/pages/projetos/suporteTecnico/cmdWin1011ReparoSO/index.html");
+    } else if (dado === 'cmdWin1011ReparoSODismSFC') {
+      window.location.replace("/pages/projetos/suporteTecnico/cmdWin1011ReparoSODismSFC/index.html");
     }
   })
 })
