5887cbdd6b16d9f73546ecad39f9c403dcd624c5
Francisco C <debian.programmer.francisco@gmail.com>
2025-11-27 10:53:56 -0300

    remoção do projeto anterior e inclusão de um novo

--- pages/projetos/aluraImersao202511/scripts/conteudo.js
+++
@@ -1,9 +0,0 @@
-cartas =document.querySelectorAll(".carta")
-
-cartas.forEach(carta => {
-  carta.addEventListener('click', (e) => {
-    dado = carta.dataset.id //Identificar qual carta foi chamada
-    if (dado === "calculadora")
-      window.location.assign('../pages/calculadora.html')
-  })
-});
